4f9da482b090f9ca71ba27c597a0b4abefddd2c7
William Cathey Diederich <wdiederi@student02.cse.nd.edu>
2018-04-26 14:20:56 -0400

    wrote function that pulls all of the body links from a wiki article

---
+++ wikiscraper.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import requests
+import re
+
+def getUrls(url):
+	r = requests.get(url)
+	while r.status_code != 200:
+		print("Connection failed")
+		r = requests.get(url)
+	print("Connection successful")
+
+	regex = r'<a href="(/wiki/[^"]+)".*</a>'
+	urls = {}
+
+	for link in re.findall(regex, r.text):
+		urls['https://en.wikipedia.org'+link] = ''
+	
+	for link in urls:
+		print(link)
+
+first = "https://en.wikipedia.org/wiki/English_language"
+
+getUrls(first)
+
